7c0ed12f5d9759faab81e40ba9dce811f6f545dd
gillotmathieu44 <mathieu-gillot-mg@outlook.fr>
2022-03-19 13:45:33 +0100

    ajout modele projet 3

---
+++ 3-DarkMode/script.js
@@ -0,0 +1,8 @@
+console.log('connecté!');
+
+const icone = document.querySelector('.btn');
+console.log(icone);
+
+icone.addEventListener('click', function(){
+    console.log("icone cliqué !");
+});
