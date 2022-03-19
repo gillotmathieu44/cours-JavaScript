c55e2a56415ccdd571dbd71a5451f370e66425b4
gillotmathieu44 <mathieu-gillot-mg@outlook.fr>
2022-03-19 14:29:36 +0100

    dossier projet 3 base

--- 3-DarkMode/script.js
+++ 3-DarkMode/script.js
@@ -1,8 +1,22 @@
 console.log('connecté!');
 
-const icone = document.querySelector('.btn');
+// je selectionneet je stocke
+// la div switch
+const switchBox = document.querySelector('.switch');
+console.log(switchBox);
+
+// la div btn (le cercle)
+const btn = document.querySelector('.btn');
+console.log(btn);
+
+//l'icone
+const icone = document.querySelector('i');
 console.log(icone);
 
-icone.addEventListener('click', function(){
-    console.log("icone cliqué !");
-});
+// la div container
+const container = document.querySelector('.container');
+console.log(container);
+
+//le titre
+const titre = document.querySelector('h1');
+console.log(titre);
