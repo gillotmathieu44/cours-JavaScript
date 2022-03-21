b9f787a1bf5b9ac5267502e12cbe1f68d8b45945
gillotmathieu44 <mathieu-gillot-mg@outlook.fr>
2022-03-21 13:19:39 +0100

    suppression ligne 3-8 script.js

--- modèle/script.js
+++ modèle/script.js
@@ -1,8 +1,1 @@
 console.log('connecté!');
-
-const icone = document.querySelector('.btn');
-console.log(icone);
-
-icone.addEventListener('click', function(){
-    console.log("icone cliqué !");
-});
