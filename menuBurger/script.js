b53a722194e20b253024f05943ae2bcb182d6de1
gillotmathieu44 <mathieu-gillot-mg@outlook.fr>
2022-03-21 15:15:28 +0100

    projet menuburger finnal

--- menuBurger/script.js
+++ menuBurger/script.js
@@ -1,1 +1,16 @@
-console.log('connecté!');
+console.log('connecté'); 
+
+// Je sélectionne et je stocke
+// l'icône burger
+const icone = document.querySelector('.navbar-mobile i'); 
+console.log(icone); 
+// la DIV modal
+const modal = document.querySelector('.modal'); 
+console.log(modal); 
+
+icone.addEventListener('click', function(){
+    console.log("icone cliquée"); 
+    modal.classList.toggle('change-modal'); 
+    icone.classList.toggle('fa-times'); 
+}); 
+
